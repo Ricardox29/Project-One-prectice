e8438ed9afa9019ff56a6cf589a2adf33c2706e5
Albeirox29 <albeiroricardovalverde000@gmail.com>
2024-05-31 11:30:15 -0500

    Fourdth commit stared project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+  const App = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <img src=".\src\Imagenes\LogoNike.png" alt="Logo Nike" style={{ width: '300px', height: 'auto' }} />
+        </div>
+
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Location</a></li>
+          <li><a href="">Abaot</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+
+      <button>Login</button>
+      </nav>
+    </div> 
+  );
+};
+
+export default App
